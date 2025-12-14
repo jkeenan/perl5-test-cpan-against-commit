cd832beda4952e67fefd4345e2f1c7bab2ae55df
James E Keenan <jkeenan@cpan.org>
2025-12-14 10:01:56 -0500

    CPAN version 0.17

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.15', new => '0.16' },
-    date    => { old => 'September 18 2025', new => 'September 26 2025' },
+    version => { old => '0.16', new => '0.17' },
+    date    => { old => 'September 26 2025', new => 'December 14 2025' },
     copyright => { old => '2025', new => '2025' },
 );
 
--- lib/Test/Against/Commit.pm
+++ lib/Test/Against/Commit.pm
@@ -1,7 +1,7 @@
 package Test::Against::Commit;
 use strict;
 use 5.14.0;
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 # core modules
 use Archive::Tar;
 use Carp;
